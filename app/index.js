41bd94f8ad9773806fb1cb4fd7c789204c072818
avantikadwivedi <avantika.dwi.1711@gmail.com>
2016-03-20 17:47:54 +0530

    add motivation package

--- app/index.js
+++ app/index.js
@@ -2,16 +2,24 @@
 
 var express = require('express');
 var app = express();
+const motivations = require('motivations');
 
 app.get('/', function(request, response) {
 	
-	response.send("hellooo");		
+	response.send("hello");		
 });
 
 app.get('/hi', function(request, response) {
 
         response.send("hi");
 });
+
+app.get('/motivations', function(request, response) {
+
+	var rand = Math.random() * (20 - 0) + 0;
+        response.send(motivations[rand]);
+});
+
 
 module.exports = app;
 
